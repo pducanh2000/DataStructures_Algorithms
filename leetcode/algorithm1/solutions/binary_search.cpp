b29d27cb593e4c2759176d38126fc8a270c1b57e
pducanh2000 <pducanh2000@gmail.com>
2022-07-23 11:45:25 +0700

    update solutions~

--- leetcode/algorithm1/solutions/binary_search.cpp
+++ leetcode/algorithm1/solutions/binary_search.cpp
@@ -1,5 +1,24 @@
-#include<iostream>
-
-int main(){
-
-}
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int begin=0, end=nums.size()-1;
+        while (end-begin > 1){
+            int mid = (end+begin)/2;
+            if (target > nums[mid]){
+                begin = mid+1;
+            }
+            else{
+                end = mid;
+            }
+        }
+        if (target == nums[begin]){
+            return begin;
+        }
+        else if(target == nums[end]){
+            return end;
+        }
+        else {
+            return -1;
+        }
+    }
+};
